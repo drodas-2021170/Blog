543612fe7f242b6244528ec6ac6a67d97d68e685
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 09:22:56 -0600

    Se agrega validaciones a las rutas de publication

--- src/publication/publication.routes.js
+++ src/publication/publication.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { addPublication } from "./publication.controller.js";
+import { addPublicationValidator } from "../../middlewares/validators.js";
 
 const api = Router();
 
-api.post('/addPost',addPublication )
+api.post('/addPost',[addPublicationValidator],addPublication )
 
 export default api
