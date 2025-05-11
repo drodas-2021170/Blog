3a8440794dc64025ca8867a19f2a18f7b098ad75
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 09:22:26 -0600

    Se agrega validaciones a las rutras de comment

--- src/comment/comment.routes.js
+++ src/comment/comment.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addComment } from "./comment.controller.js";
+import { addCommentValidator } from "../../middlewares/validators.js";
 
 
 const api = Router();
 
-api.post('/addComment', addComment)
+api.post('/addComment',[addCommentValidator], addComment)
 
 export default api
