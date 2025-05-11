db34e09b347d88175ae7890bc57e09b85bf763a0
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 09:42:55 -0600

    Se agrega nueva ruta para getPublications

--- src/publication/publication.routes.js
+++ src/publication/publication.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addPublication } from "./publication.controller.js";
+import { addPublication, getPublications } from "./publication.controller.js";
 import { addPublicationValidator } from "../../middlewares/validators.js";
 
 const api = Router();
 
-api.post('/addPost',[addPublicationValidator],addPublication )
+api.post('/addPublication',[addPublicationValidator],addPublication )
+api.get('/getPublications', getPublications)
 
 export default api
