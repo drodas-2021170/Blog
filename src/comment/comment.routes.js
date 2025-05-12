47fd6ea0b47f5ca2fb1bbb833712ebb7a8c4f17a
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-11 19:12:02 -0600

    Se modifica ruta de la función getCommentsByPublication

--- src/comment/comment.routes.js
+++ src/comment/comment.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addComment } from "./comment.controller.js";
+import { addComment, getCommentsByPublication } from "./comment.controller.js";
 import { addCommentValidator } from "../../middlewares/validators.js";
 
 
 const api = Router();
 
 api.post('/addComment',[addCommentValidator], addComment)
+api.get('/getCommentsByPublication/:id',getCommentsByPublication)
 
 export default api
