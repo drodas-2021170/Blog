e1a972ab4b396b83b9af6068c7693523a7a33759
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-12 21:18:56 -0600

    Se modifica archivo

--- src/publication/publication.controller.js
+++ src/publication/publication.controller.js
@@ -14,7 +14,7 @@ export const addPublication = async (req, res) => {
 
 export const getPublications = async(req,res)=>{
     try {
-        let publications = await Publication.find().populate('course')
+        let publications = await Publication.find()
         if(!publications) return res.status(404).send({sucess:false, message:'Publications not found'})
         return res.send({success:true, message:'Publications found', publications})
     } catch (err) {
