a9b626880c9594a5c317967b5d37cb0cc3828f3c
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-14 18:09:22 -0600

    Se agrega nueva ruta

--- src/publication/publication.routes.js
+++ src/publication/publication.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addPublication, getPublications } from "./publication.controller.js";
+import { addPublication, getPublications, getPublicationsById } from "./publication.controller.js";
 import { addPublicationValidator } from "../../middlewares/validators.js";
 
 const api = Router();
 
 api.post('/addPublication',[addPublicationValidator],addPublication )
 api.get('/getPublications', getPublications)
+api.get('/getPublication/:id', getPublicationsById)
 
 export default api
